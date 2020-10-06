c204df305834b6fb0b815921b912e231ce366945
Joe Brown <brwnjm@gmail.com>
2020-10-05 19:42:01 -0600

    indentation

--- templates/idplot.py
+++ templates/idplot.py
@@ -467,7 +467,7 @@ TEMPLATE = """
     );
 
     const data = {{data}}
-        const cov_color = 'rgba(108,117,125,0.2)'
+    const cov_color = 'rgba(108,117,125,0.2)'
     const colors = [
         "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b",
         "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", "#7CB5EC", "#434348",
